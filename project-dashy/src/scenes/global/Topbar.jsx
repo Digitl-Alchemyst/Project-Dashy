dd19e9b0811f8253db9d50bd1c4c3b8ce962062b
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-03 20:41:47 -0500

    First commit initial layout

---
+++ project-dashy/src/scenes/global/Topbar.jsx
@@ -0,0 +1,55 @@
+// import Core components
+import { useContext } from 'react';
+// import context
+import { ColorModeContext, tokens } from '../../theme';
+// import MUI components
+import { Box, IconButton, useTheme } from '@mui/material';
+import { InputBase } from '@mui/material';
+// import MUI Icons
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+
+      {/* Search Box */}
+      <Box display="flex" bgcolor={colors.primary[400]} borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchOutlinedIcon />
+        </IconButton>
+      </Box>
+
+      {/* Icon Buttons */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
